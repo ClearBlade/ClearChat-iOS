c2437c7a6124332b99672c1458224ffa5b80fba9
Tyler Dodge <tldodge@ncsu.edu>
2013-10-29 23:06:16 -0400

    Fixed license to be Clearblade instead of Tyler dodge on two files

--- chatNavigationViewController.h
+++ chatNavigationViewController.h
@@ -3,7 +3,7 @@
 //  Chat
 //
 //  Created by Tyler Dodge on 10/25/13.
-//  Copyright (c) 2013 Tyler Dodge. All rights reserved.
+//  Copyright (c) 2013 Clearblade. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
